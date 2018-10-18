cfa0675ce8ac0d6bdc42f3828a3e1bc067600cab
jonathan <jonathan@jonathan-HP-ZBook-Studio-G3-ubuntu16.04>
2018-10-18 08:12:13 +0200

    add data analysis...

--- Kyb_prog/indoor_measurment/manual_indoor/plot_improvment.py
+++ Kyb_prog/indoor_measurment/manual_indoor/plot_improvment.py
@@ -14,10 +14,12 @@ def function(file_name_arg):
 
   file_name = file_name_arg
   #  Define waypoints
-  Strecke_1 = [[1, -1.5],
-               [3.7, -1.5],
-               [3.7,-6],
-               [1,-6]]
+  # Strecke_1 = [[1.0, -1.5],
+  #               [3.7, -1.5],
+  #              [3.7, -6],
+  #              [1.0, -6]]
+  Strecke_1 = [[3.7, -1.5],
+               [3.7, -6]]
   numberPoints = len(Strecke_1) # number points of Ground_Truth
   data_soll = Strecke_1;
 
@@ -67,20 +69,20 @@ if __name__== "__main__":
 
     plt.rcParams.update({'font.size': 30})
 
-    file_name = 'Lmap2.csv'
+    file_name = 'comeBack1-base_link_odom_camera_is1500.csv'
     err_min_1, err_min_1, n_1, Strecke_1 = function(file_name)
     # print(Strecke_1[int(1)][int(1)])
     f = plt.figure(1, figsize=(40, 32))
     ax = f.add_subplot(111)
-    # for i in range(0, len(Strecke_1), 1):
-        # plt.plot(Strecke_1[int(i)][int(0)], Strecke_1[int(i)][int(1)], 'o' ,color='red', markersize=20, label="Ground truth Point " +str(i+1) + ": "+str(Strecke_1[i]))
-        # plt.plot(base_link_x_odom_camera[int(n_1[i])], base_link_y_odom_camera[int(n_1[i])], 'o' ,color='blue', markersize=20, label="Point " +str(i+1) + ": " + str(err_min_1[i]) + " m")
-        # plt.annotate(
-        #     str('( ' + str(Strecke_1[int(i)][int(0)]) +', ' + str(Strecke_1[int(i)][int(1)]) + ')'),
-        #     xy=((Strecke_1[int(i)][int(0)]+0.0), (Strecke_1[int(i)][int(1)])+0.0), xytext=(-10, 10),
-        #     textcoords='offset points', ha='right', va='bottom',
-        #     bbox=dict(boxstyle='round,pad=0.5', fc='grey', alpha=0.5),
-        #     arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
+    for i in range(0, len(Strecke_1), 1):
+        plt.plot(Strecke_1[int(i)][int(0)], Strecke_1[int(i)][int(1)], 'o' ,color='red', markersize=20, label="Ground truth Point " +str(i+1) + ": "+str(Strecke_1[i]))
+        plt.plot(base_link_x_odom_camera[int(n_1[i])], base_link_y_odom_camera[int(n_1[i])], 'o' ,color='blue', markersize=20, label="Point " +str(i+1) + ": " + str(err_min_1[i]) + " m")
+        plt.annotate(
+            str('( ' + str(Strecke_1[int(i)][int(0)]) +', ' + str(Strecke_1[int(i)][int(1)]) + ')'),
+            xy=((Strecke_1[int(i)][int(0)]+0.0), (Strecke_1[int(i)][int(1)])+0.0), xytext=(-10, 10),
+            textcoords='offset points', ha='right', va='bottom',
+            bbox=dict(boxstyle='round,pad=0.5', fc='grey', alpha=0.5),
+            arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
     plt.plot(base_link_x_odom_camera, base_link_y_odom_camera, label='Robot position in camera frame')
     # plt.plot(base_link_y_odom_camera_tf, base_link_x_odom_camera_tf, label='Camera position in Base link frame with tf')
     # plt.plot(base_camera_y_odom_camera, base_camera_x_odom_camera, label='Camera position in Base camera frame')
@@ -91,14 +93,16 @@ if __name__== "__main__":
 
     plt.xlabel('x [m]')
     plt.ylabel('y [m]')
+    plt.xlim(0.0, 6.0)
+    plt.ylim(0.0,- 8.0)
     # plt.title('Measurment of the odometry' + '\n' + file_name)
     # plt.legend()
     plt.legend(numpoints=1, bbox_to_anchor=(0., 1.02, 1., .102), loc=1,
-               ncol=3, mode="expand", borderaxespad=0.)#bbox_to_anchor=(0., 1.02, 1., .102)
+               ncol=3, mode="expand", borderaxespad=0.) #bbox_to_anchor=(0., 1.02, 1., .102)
     # plt.legend(numpoints=1, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     # plt.legend(numpoints=1, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     # f.set_size_inches(20, 15)
-    f.savefig(file_name+'.png')
+    # f.savefig(file_name+'.png')
     # plt.close(f)
 
     plt.show()
